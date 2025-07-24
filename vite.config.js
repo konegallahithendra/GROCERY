da557c7527f4338c3301225060452874f09a9ca9
konegallahithendra <konegallahithendra2005@gmail.com>
2025-07-24 09:45:46 +0530

    tailwind added

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './',  // <-- Add this line
+  base: './', // ✅ Required for Render
   plugins: [react()],
 })
